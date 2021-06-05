5372f9dded97bced0cb8373d7a0ff8b9746dc276
hui <2228157440@qq.com>
2021-06-05 22:27:57 +0800

    新增Python注释测试代码

---
+++ code/day02-Python注释/03-Python注释.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author: Hui
+# @Desc: { Python注释测试 }
+# @Date: 2021/06/03 23:03
+
+# 这是第一个单行注释
+print("hello python")
+
+print("hello python")  # 输出 hello python
+
+# 使用多个单行注释来多行注释
+# 输入 hello world
+print('hello world')
+
+"""
+这是一个多行注释
+
+在多行注释之间，可以写很多很多的内容……
+"""
+print("hello python")
